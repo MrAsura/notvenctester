650acc9e4a807012ee8ec4c8c6de80acc30ee7f7
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2017-04-24 15:14:08 +0300

    Initial commit

---
+++ notvenctester/notvenctester/TestInstance.py
@@ -0,0 +1,19 @@
+﻿class TestInstance(object):
+    """
+    Create a test instance object
+    @param layer_args: Specify parameters not give as separate args (iterable with an element for each layer)
+    @param layer_sizes: Specify layer sizes (iterable with an element for each layer)
+    @param inputs: Specify input files for each input
+    @param input_sizes: Specify sizes for each input (if not in filename)
+    @return self object
+    """
+    def __init__(self, layer_args, layer_sizes, inputs, input_sizes, *args, **kwargs):
+        self._layer_sizes = layer_sizes
+        self._layer_args = layer_args
+        self._inputs = inputs
+        self._input_sizes = input_sizes
+        return self
+
+
+
+
---
+++ notvenctester/notvenctester/notvenctester.py
@@ -0,0 +1,8 @@
+"""
+Test suite for testing SHVC kvazaar
+"""
+
+import TestInstance
+
+if __name__ == "__main__":
+    print("Start tests...")
